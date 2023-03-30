6cd6d484877dd37d9fc6d63d4c1c3864be75e0ca
katlego <katlego@accsight.org>
2023-03-30 05:23:47 +0200

    can read from csv to db

---
+++ controllers/testCSV2.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const csvParser = require('csv-parser');
+const mongoose = require('mongoose');
+require('../models/products');
+const Prod = mongoose.model('Product');
+
+const sendJSONResponse = (res, stat, content) => {
+    res
+      .status(stat)
+      .json(content);
+};
+
+const createProduct = (res, data) => {
+    const trade = Number(data.trade);
+    const retail = trade*0.3 + trade;
+    console.log(trade);
+    Prod.create({
+        name: data.name,
+        description: data.description,
+        trade: trade,
+        selling: retail,
+        userId: data.userId
+    },(err, product)=>{
+        if(err) {
+            console.log(err);
+            sendJSONResponse(res, 400, err);
+        } else {
+            console.log(product);
+            //sendJSONResponse(res, 201, product);
+        }
+    });
+};
+
+
+const csvTester2 = (req, res) => {
+    const products = [];
+    fs.createReadStream('./testD.csv')
+      .pipe(csvParser({separator: ';'}))
+      .on("data", (data)=> {
+        products.push(data);
+        createProduct(res, data);
+        })
+      .on("end", ()=>{
+        console.log(products);
+      });
+
+    res.render('index', 
+    { 
+        title: 'Expression',
+        products 
+    });
+};
+
+module.exports = {
+    csvTester2
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-var csvCtrl = require('../controllers/testCSV');
+var csvCtrl = require('../controllers/testCSV2');
 
 /* GET home page. */
-router.get('/', csvCtrl.csvTester);
+router.get('/', csvCtrl.csvTester2);
 
 module.exports = router;
